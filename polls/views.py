e32d9a0d92ac5cb43ae49911598d87960ecef19a
Balram <singhbalram054@gmail.com>
2019-09-22 19:57:11 +0530

    djanjo vies api

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+'''
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
@@ -19,3 +20,24 @@ def polls_detail(request):
 	"pub_date": poll.pub_date 
 	}}
 	return JsonResponse(data)
+	'''
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Poll, Choice
+from .serializers import PollSerializer
+
+class PollList(APIView):
+	def get(self, request):
+		polls = Poll.objects.all()[:20]
+		data = PollSerializer(polls, many=True).data
+		return Response(data)
+
+
+
+class PollDetail(APIView):
+	def get(self, request, pk):
+		poll = get_object_or_404(Poll, pk=pk)
+		data = PollSerializer(poll).data
+		return Response(data)
